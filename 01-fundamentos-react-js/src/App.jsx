1d0b4f34fab29935cfd3b8810c52344c1813ae03
AndreLucasv10 <88734289+AndreLucasv10@users.noreply.github.com>
2025-03-19 23:19:51 -0300

    commit

--- 01-fundamentos-react-js/src/App.jsx
+++ 01-fundamentos-react-js/src/App.jsx
@@ -1,8 +1,10 @@
 import {Post} from './Post';
+import { Header } from './Components/Header';
 import './style.css'
 function App() {
   return (
     <>
+      <Header />
       <Post 
         autor='André Lucas' 
         content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta voluptas inventore dolorem ex maiores ea quod, tempore odio enim dolor tempora dolores doloremque rem et nesciunt consequatur delectus reprehenderit nam."
