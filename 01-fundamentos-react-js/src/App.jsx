f19be6e2b6552323d788685d2b6543a02943a7a2
AndreLucasv10 <88734289+AndreLucasv10@users.noreply.github.com>
2025-03-19 22:01:21 -0300

    first commit

---
+++ 01-fundamentos-react-js/src/App.jsx
@@ -0,0 +1,14 @@
+import {Post} from './Post';
+import './style.css'
+function App() {
+  return (
+    <>
+      <Post 
+        autor='André Lucas' 
+        content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta voluptas inventore dolorem ex maiores ea quod, tempore odio enim dolor tempora dolores doloremque rem et nesciunt consequatur delectus reprehenderit nam."
+        />
+    </>
+  )
+}
+
+export default App
---
+++ 01-fundamentos-react-js/src/Post.jsx
@@ -0,0 +1,8 @@
+ export function Post({autor, content}){
+    return (
+        <>
+            <strong>{autor}</strong>
+            <p>{content}</p>
+        </>
+    )
+}
